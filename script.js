5a1ae1cc0742ac6766b956ff0b93ee1c2ff13a6b
Gdgiri <giridharan831@gmail.com>
2024-05-22 18:22:04 +0530

    update

--- script.js
+++ script.js
@@ -8,21 +8,25 @@ var greet;
 
 if (hrs < 12) {
   greet = "Good Morning";
-  bg.style = `background-image: url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hdwallpapers.in%2Fdownload%2Fmountain_with_clouds_background_during_dawn_morning_4k_hd_nature-HD.jpg&f=1&nofb=1&ipt=13e9cbfd180f4d2af9eb8990f154a3681fe268ac7f9d63848aa8ccd905d564e6&ipo=images")`;
-  image.innerHTML = `<img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FUI1LUP61p3YYM%2Fgiphy.gif&f=1&nofb=1&ipt=ebddaa3212e59fa7755881fd95a5bd38ca77b0e4f31343beedf9ba77812e6f1a&ipo=images" alt="good morning">`;
+  image.innerHTML = ` <h1>GOOD MORNING</h1>
+  <img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FUI1LUP61p3YYM%2Fgiphy.gif&f=1&nofb=1&ipt=ebddaa3212e59fa7755881fd95a5bd38ca77b0e4f31343beedf9ba77812e6f1a&ipo=images" alt="good morning">
+  `;
 } else if (hrs >= 12 && hrs <= 16) {
   greet = "Good Afternoon";
-  bg.style = `background-image:url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp7903174.jpg&f=1&nofb=1&ipt=af607857dd3403a901b8e8bfc0d9720f8f4a3a075045f9d084b5993f0b17701f&ipo=images")`;
-
-  image.innerHTML = `<img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FxT0Gqz4x4eLd5gDtaU%2Fgiphy.gif&f=1&nofb=1&ipt=14a20a377304aae0326a1f3adf43c301b033f881536de2edd765922ce0db1bbe&ipo=images" alt="good afternoon">`;
+  image.innerHTML = ` <h1>GOOD AFTERNOON</h1>
+  <img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FxT0Gqz4x4eLd5gDtaU%2Fgiphy.gif&f=1&nofb=1&ipt=14a20a377304aae0326a1f3adf43c301b033f881536de2edd765922ce0db1bbe&ipo=images" alt="good afternoon">
+  `;
 } else if (hrs >= 16 && hrs <= 20) {
   greet = "Good Evening";
-  image.innerHTML = `<img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia0.giphy.com%2Fmedia%2FdP8DxhD9ekk1i%2Fgiphy.gif&f=1&nofb=1&ipt=a9aeab352bf0a7f287a64d414d9718295f44a2a63afe3632a6af40a87ddcb388&ipo=images" alt="good evening" width="800px">`;
+  image.innerHTML = `
+  <h1>GOOD EVENING</h1>
+  <img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia0.giphy.com%2Fmedia%2FdP8DxhD9ekk1i%2Fgiphy.gif&f=1&nofb=1&ipt=a9aeab352bf0a7f287a64d414d9718295f44a2a63afe3632a6af40a87ddcb388&ipo=images" alt="good evening" width="800px">
+  `;
 } else if (hrs >= 20 && hrs <= 24) {
   greet = "Good Night";
-  bg.style = `background-image: url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hdwallpapers.in%2Fdownload%2Fmountain_with_clouds_background_during_dawn_morning_4k_hd_nature-HD.jpg&f=1&nofb=1&ipt=13e9cbfd180f4d2af9eb8990f154a3681fe268ac7f9d63848aa8ccd905d564e6&ipo=images")`;
-
-  image.innerHTML = `<img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2Fb87c9897b5c864467537652e56dd4822%2Ftenor.gif%3Fitemid%3D14745168&f=1&nofb=1&ipt=6211779c9e2b2076a2b04557ddae8b62f317489349755a8caa1832aed2847c77&ipo=images" alt="good night">`;
+  image.innerHTML = `
+  <h1>GOOD NIGHT</h1><img src=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2Fb87c9897b5c864467537652e56dd4822%2Ftenor.gif%3Fitemid%3D14745168&f=1&nofb=1&ipt=6211779c9e2b2076a2b04557ddae8b62f317489349755a8caa1832aed2847c77&ipo=images" alt="good night">
+ `;
 }
 greeting.innerHTML = "<b>" + greet + "</b>";
 
